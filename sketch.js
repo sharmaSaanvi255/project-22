2aa36e7d3e39ae0f7123c534b7649a17f6e3a06f
sharmaSaanvi255 <72978396+sharmaSaanvi255@users.noreply.github.com>
2020-11-19 12:17:25 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -30,14 +30,13 @@ function setup() {
 
 	engine = Engine.create();
 	world = engine.world;
-
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
-	World.add(world, packageBody);
 	
-
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
+	 World.add(world, ground);
+	 
+	 packageBody=Bodies.circle(width/2, 200 ,5, {restitution : 3,isStatic=true})
+	 World.add(packageBody,world);
 
 
 	Engine.run(engine);
@@ -58,7 +57,7 @@ function keyPressed() {
  if (keyCode === DOWN_ARROW) {
 	// Look at the hints in the document and understand how to make the package body fall only on
 	
-	parcel=body.circle(width/2,200,5,{isStatic:false});
+	Body.setStatic(packageBody,false);
   }
 }
 
